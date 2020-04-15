f946dacfeb7d2351a94d73d83393f926435ff483
Alex <alexdukmak@hotmail.com>
2020-04-15 12:16:29 +0300

    First Push

---
+++ Desktop/Spring 2020/SWER 313/SWER 313 Project/soap/src/main/java/edu/bu/soap/SoapProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.bu.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapProjectApplication.class, args);
+	}
+
+}
---
+++ Desktop/Spring 2020/SWER 313/SWER 313 Project/soap/src/test/java/edu/bu/soap/SoapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bu.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
