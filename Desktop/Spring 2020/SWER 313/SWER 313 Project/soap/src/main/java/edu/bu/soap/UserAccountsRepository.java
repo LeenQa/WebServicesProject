c3f36b3eff56838d9ca25917eaa088241630c0ee
Alex <alexdukmak@hotmail.com>
2020-04-21 23:05:13 +0300

    idk

--- Desktop/Spring 2020/SWER 313/SWER 313 Project/soap/src/main/java/edu/bu/soap/UserAccountsRepository.java
+++ Desktop/Spring 2020/SWER 313/SWER 313 Project/soap/src/main/java/edu/bu/soap/UserAccountsRepository.java
@@ -1,8 +1,11 @@
 package edu.bu.soap;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserAccountsRepository extends JpaRepository<UserAccounts, String> {
+	Optional<UserAccounts> findByUserName(String UserName);
 
 }
